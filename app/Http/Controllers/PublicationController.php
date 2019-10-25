d49c2136e0350594548415ff17a0caa162f86deb
jinson7 <jinsonp-7@hotmail.com>
2019-10-25 02:55:53 +0200

    #149 finalizada con la relación 1..* con la tabla users y delete cascade.

---
+++ app/Http/Controllers/PublicationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\publication;
+use Illuminate\Http\Request;
+
+class PublicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function show(publication $publication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(publication $publication)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, publication $publication)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(publication $publication)
+    {
+        //
+    }
+}
