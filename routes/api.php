ff0536b6a457e258121392edc4a0b4c86b4fcaca
David Luque <luquee48@gmail.com>
2019-10-25 13:23:29 +0200

    init for work in branch

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ Route::post('upload_photo', 'FileController@upload_photo');
 Route::post('user/{username}/set_configurations', 'Api\UserController@set_configurations');
 Route::get('user/{username}', 'Api\UserController@get_info_user');
 Route::put('user/{username}/update', 'Api\UserController@update_info_user');
+
+
+// Publications
+Route::resource('publication', 'PublicationController');
