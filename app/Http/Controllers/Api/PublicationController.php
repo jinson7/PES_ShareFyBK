634bd74e9321c2eb2eebf2ac3a2449fb10f60b66
David Luque <luquee48@gmail.com>
2019-10-26 17:19:26 +0200

    #156 - acabada

--- app/Http/Controllers/Api/PublicationController.php
+++ app/Http/Controllers/Api/PublicationController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
+use App\Publication;
 
 class PublicationController extends Controller
 {
     
     public function __construct(){
-        //$this->middleware('jwt');
+        $this->middleware('jwt');
     }
 
     /**
@@ -34,14 +35,84 @@ class PublicationController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+     * @OA\Post(
+     *     path="/api/publication?token=valor",
+     *     tags={"publication"},
+     *     summary="Es crea la publicació amb la informació enviada.",
+     *     description="Es crea la publicació amb la informació enviada.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna un json amb el missatge 'operació correcta' "
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="El vídeo no té un format vàlid, format suportat .mp4"
+     *     ),
+     *     @OA\Parameter(
+     *         name="id_user",
+     *         in="query",
+     *         description="string amb el id de l'usuari",
+     *         required=true
+     *     ),
+     *     @OA\Parameter(
+     *         name="game",
+     *         in="query",
+     *         description="string amb el valor del game",
+     *         required=true
+     *     ),
+     *     @OA\Parameter(
+     *         name="video",
+     *         in="query",
+     *         description="valor del video en el form",
+     *         required=true
+     *     ),
+     *     @OA\Parameter(
+     *         name="text",
+     *         in="query",
+     *         description="string amb el text de la publicació",
+     *         required=true
+     *     ),
+     *     @OA\Parameter(
+     *         name="token",
+     *         in="query",
+     *         description="Valor del token_access",
+     *         required=true
+     *     )
+     * )
+    */
     public function store(Request $request)
     {
-        //
+
+        // agafar el video
+        $file = $request->file('video');
+        $ext = $file->getClientOriginalExtension();
+
+        if ( $ext === 'mp4' ) {
+            // crear publicació
+            $publication = Publication::create([
+                'id_user' => $request->id_user,
+                'game' => $request->game,
+                'text' => $request->text
+            ]);
+
+            // crear ruta per el clip
+            $id_publication = str_pad($publication->id, 3, "0", STR_PAD_LEFT);
+            $path = '/media/clips/'.$id_publication[0].'/'.$id_publication[1].'/'.$id_publication[2].'/';
+        
+            $date = now()->timestamp;
+            $name_file = $date . '.' . $ext;
+            $file->move(public_path($path), $name_file);
+            $publication->video_path = $path.$name_file;
+            $publication->save();
+            return response()->json([
+                'message' => 'Publicació creada correctament.'
+            ], 200);
+        }else{
+            return response()->json([
+                'error' => "El vídeo no té un format vàlid, format suportat .mp4"
+            ], 401);
+        }
+        
     }
 
     /**
