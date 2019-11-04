803f53d26c6b3327639c80ee506e750951e3e7c3
jinson7 <jinsonp-7@hotmail.com>
2019-11-04 22:34:29 +0100

    #191 Add Table Like

---
+++ app/Http/Controllers/Api/LikeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Like;
+
+class LikeController extends Controller
+{
+    //
+}
