c183d35b8cc09035adce894f76c32f2f5f5ca2ae
jinson7 <jinsonp-7@hotmail.com>
2019-11-04 23:29:54 +0100

    #193 Add Table Comments

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Comment;
+
+class CommentController extends Controller
+{
+    //
+}
---
+++ database/migrations/2019_11_04_215835_create_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigInteger('id_user')->unsigned()->anullable();
+            $table->bigInteger('id_publication')->unsigned()->anullable();
+            $table->timestamp('date')->anullable();
+            $table->text('text')->anullable();
+            $table->timestamps();
+
+            $table->primary(['id_user', 'id_publication', 'date']);
+
+            $table->foreign('id_user')->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->foreign('id_publication')->references('id')->on('publications')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
