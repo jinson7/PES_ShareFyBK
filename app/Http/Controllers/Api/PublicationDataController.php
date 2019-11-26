337be5ae159b8d11f73225251298ef1f47f8e0d5
jinson7 <jinsonp-7@hotmail.com>
2019-11-26 23:59:43 +0100

    #219 Add Data Layer of Publication.

---
+++ app/Http/Controllers/Api/PublicationDataController.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use App\Http\Controllers\Controller;
+use App\Publication;
+use App\User;
+use App\Like;
+
+class PublicationDataController extends Controller
+{
+    /**
+     * @OA\Get(
+      *     path="/api/user/{id_user}/publications",
+      *     tags={"publication"},
+      *     summary="Dado un id de usuario existente, devuelve todas sus publicaciones",
+      *     description="Dado un id de usuario existente, devuelve todas sus publicaciones",
+      *     @OA\Response(
+      *         response=200,
+      *         description="Devuelve un json con la información de la publicación."
+      *     ),
+      *     @OA\Parameter(
+      *         name="token",
+      *         in="query",
+      *         description="Valor del token_access",
+      *         required=true
+      *     )
+     * )
+      */
+    public function list_user($id_user){
+        $publications = Publication::where('id_user', $id_user)->get();
+        return response()->json([
+            'value' => $publications
+        ], 200);
+    }
+
+    /**
+     * @OA\Post(
+      *     path="/api/publication",
+      *     tags={"publication"},
+      *     summary="Es crea la publicació amb la informació enviada.",
+      *     description="Es crea la publicació amb la informació enviada.",
+      *     @OA\Response(
+      *         response=200,
+      *         description="Retorna un json amb el identificador de la publicació"
+      *     ),
+      *     @OA\Response(
+      *         response=401,
+      *         description="El vídeo no té un format vàlid, format suportat .mp4"
+      *     ),
+      *     @OA\Parameter(
+      *         name="id_user",
+      *         in="query",
+      *         description="string amb el id de l'usuari",
+      *         required=true
+      *     ),
+      *     @OA\Parameter(
+      *         name="game",
+      *         in="query",
+      *         description="id del game",
+      *         required=true
+      *     ),
+      *     @OA\Parameter(
+      *         name="video",
+      *         in="query",
+      *         description="valor del video en el form",
+      *         required=true
+      *     ),
+      *     @OA\Parameter(
+      *         name="text",
+      *         in="query",
+      *         description="string amb el text de la publicació",
+      *         required=true
+      *     ),
+      *     @OA\Parameter(
+      *         name="token",
+      *         in="query",
+      *         description="Valor del token_access",
+      *         required=true
+      *     )
+     * )
+    */
+    public function create(Request $request, $file, $ext) {
+        $publication = Publication::create([
+            'id_user' => $request->id_user,
+            'id_game' => $request->game,
+            'text' => $request->text
+        ]);
+        // crear ruta per el clip
+        $id_publication = str_pad($publication->id, 3, "0", STR_PAD_LEFT);
+        $path = '/media/clips/'.$id_publication[0].'/'.$id_publication[1].'/'.$id_publication[2].'/';
+        $date = now()->timestamp;
+        $name_file = $date . '.' . $ext;
+        Storage::disk('public')->putFileAs($path, $file, $name_file);
+        $publication->video_path = '/storage'.$path.$name_file;
+        $publication->save();
+        return response()->json([
+            'value' => $publication->id
+        ], 200);
+    }
+
+    /**
+     * @OA\Get(
+      *     path="/api/publication/{id}",
+      *     tags={"publication"},
+      *     summary="Dado un id de publicación existente, devuelve su información.",
+      *     description="Dado un id de publicación existente, devuelve su información.",
+      *     @OA\Response(
+      *         response=200,
+      *         description="Devuelve un json con la información de la publicación."
+      *     ),
+      *     @OA\Parameter(
+      *         name="token",
+      *         in="query",
+      *         description="Valor del token_access",
+      *         required=true
+      *     )
+     * )
+      */
+    public function show($id) {
+        $publication = Publication::where('id', $id)->first();
+        //$publication->user = User::select('username', 'photo_path')->where('id', $publication->id_user)->first();
+        $likes = Like::with('user:id,username')->where('id_publication', $publication->id)->get();
+        $list_usernames_like = $likes->implode('user.username', ',');
+        //$publication->num_likes = $likes->count();
+        $publication->likes = explode(',', $list_usernames_like);
+        return response()->json([
+            'value' => $publication
+        ], 200);
+    }
+
+    /** 
+     * @OA\Put(
+      *     path="/api/publication/{id}",
+      *     tags={"publication"},
+      *     summary="Dado un id de publicación existente, edita dicha publicación.",
+      *     description="Dado un id de publicación existente, y los campos a modificados guarda la dicha informacioón.",
+      *     @OA\Response(
+      *         response=200,
+      *         description="Devuelve un json con el mensaje: 'Publicació editada correctament'."
+      *     ),
+      *     @OA\Response(
+      *         response=400,
+      *         description="Devuelve un json con el error: 'No existe la publicación a editar'."
+      *     ),
+      *     @OA\Parameter(
+      *         name="game",
+      *         in="query",
+      *         description="Id del juego",
+      *     ),
+      *     @OA\Parameter(
+      *         name="text",
+      *         in="query",
+      *         description="Texto descriptivo de la publicación",
+      *     ),
+      *     @OA\Parameter(
+      *         name="token",
+      *         in="query",
+      *         description="Valor del token_access",
+      *         required=true
+      *     )
+      * )
+     */
+    public function update(Request $request, $id)
+    {
+        $publication = Publication::find($id);
+        if ($publication !== null) {
+            if ($request->game !== null) $publication->id_game = $request->game;
+            if ($request->text !== null) $publication->text = $request->text;
+            $publication->save();
+            return response()->json([
+                'message' => 'Publicació editada correctament.'
+            ], 200);
+        }
+        return response()->json([
+            'error' => 'No existe la publicación a editar.'
+        ], 400);
+    }
+
+    /** @OA\DELETE(
+     *     path="/api/publication/{publication_id}",
+     *     tags={"publication"},
+     *     summary="Dado un id de publicación existente, elimina dicha publicación.",
+     *     description="Dado un id de publicación existente, elimina dicha publicación.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Devuelve un json con el mensaje: 'Publicació eliminada correctament'."
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Devuelve un json con el error: 'No existeix la publicació a eliminar'."
+     *     ),
+     *     @OA\Parameter(
+     *         name="token",
+     *         in="query",
+     *         description="Valor del token_access",
+     *         required=true
+     *     )
+     * )
+     */
+    public function delete($id)
+    {
+        $publication = Publication::find($id);
+        if ($publication !== null) {
+            $video_path = $publication->video_path;
+            // quitamos el /storage de el video_path
+            $video_path = substr($publication->video_path, 8);
+            
+            if(Storage::disk('public')->exists($video_path)){
+                Storage::disk('public')->delete($video_path);
+            }
+            $publication->delete();
+
+            return response()->json([
+                'message' => 'Publicació eliminada correctament.'
+            ], 200);
+        }
+        return response()->json([
+            'error' => 'No existeix la publicació a eliminar.'
+        ], 400);
+    }
+}
